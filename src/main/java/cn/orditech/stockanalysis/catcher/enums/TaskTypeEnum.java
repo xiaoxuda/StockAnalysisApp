0f84ee05e8fc698752eea50c71966bd4288cfccb
xiaoxuda <xiaoxuda0715@163.com>
2017-04-04 17:40:52 +0800

    任务周期变更

--- src/main/java/cn/orditech/stockanalysis/catcher/enums/TaskTypeEnum.java
+++ src/main/java/cn/orditech/stockanalysis/catcher/enums/TaskTypeEnum.java
@@ -12,17 +12,35 @@ public enum TaskTypeEnum {
     /**
      * 抓取财务报表
      **/
-    EASTMONEYNET_STATEMENT ("eastmoney_statement", "财务报表", CycleEnum.WEEK),
+    EASTMONEYNET_STATEMENT ("eastmoney_statement", "财务报表", CycleEnum.DAY, new CycleRule () {
+        public boolean decide () {
+            Calendar calendar = Calendar.getInstance ();
+            //每天早上5点工作
+            return calendar.get (Calendar.HOUR_OF_DAY) == 5;
+        }
+    }),
 
     /**
      * 抓取上市公司列表
      **/
-    JUCAONET_COMPANY_LIST ("jucaonet_company_list", "股票列表", CycleEnum.WEEK),
+    JUCAONET_COMPANY_LIST ("jucaonet_company_list", "股票列表", CycleEnum.DAY, new CycleRule () {
+        public boolean decide () {
+            Calendar calendar = Calendar.getInstance ();
+            //每天早上2点工作
+            return calendar.get (Calendar.HOUR_OF_DAY) == 2;
+        }
+    }),
 
     /**
      * 获取公司股本数量
      **/
-    JUCAONET_COMPANY_SHARECAPITAL ("jucaonet_company_sharecapital", "股本", CycleEnum.DAY),
+    JUCAONET_COMPANY_SHARECAPITAL ("jucaonet_company_sharecapital", "股本", CycleEnum.DAY, new CycleRule () {
+        public boolean decide () {
+            Calendar calendar = Calendar.getInstance ();
+            //每天早上4点工作
+            return calendar.get (Calendar.HOUR_OF_DAY) == 4;
+        }
+    }),
 
     /**
      * 获取股票当前交易信息
@@ -32,18 +50,24 @@ public enum TaskTypeEnum {
             Calendar calendar = Calendar.getInstance ();
             int dayOfWeek = calendar.get (Calendar.DAY_OF_WEEK);
             //周末不开市啊！！！
-            if(dayOfWeek == Calendar.SATURDAY || dayOfWeek == Calendar.SUNDAY){
+            if (dayOfWeek == Calendar.SATURDAY || dayOfWeek == Calendar.SUNDAY) {
                 return false;
             }
             //每天工作八小时
-            return calendar.get(Calendar.HOUR_OF_DAY)>=8 && calendar.get(Calendar.HOUR_OF_DAY)<=16;
+            return calendar.get (Calendar.HOUR_OF_DAY) >= 8 && calendar.get (Calendar.HOUR_OF_DAY) <= 16;
         }
     }),
 
     /**
      * 获取股票历史交易信息
      **/
-    SINAJS_HISTORY_TRADE_DETAIL ("sinajs_history_trade_detail", "股票历史交易信息", CycleEnum.MONTH);
+    SINAJS_HISTORY_TRADE_DETAIL ("sinajs_history_trade_detail", "股票历史交易信息", CycleEnum.DAY, new CycleRule () {
+        public boolean decide () {
+            Calendar calendar = Calendar.getInstance ();
+            //每天晚上十点开始工作
+            return calendar.get (Calendar.HOUR_OF_DAY) == 22;
+        }
+    });
 
     /**
      * 任务代码
