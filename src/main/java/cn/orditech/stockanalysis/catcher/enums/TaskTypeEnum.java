7d0521b6a64e89df1ba54d2387a69bd469dc5c53
xiaoxuda <xiaoxuda0715@163.com>
2017-05-07 21:33:52 +0800

    fix

--- src/main/java/cn/orditech/stockanalysis/catcher/enums/TaskTypeEnum.java
+++ src/main/java/cn/orditech/stockanalysis/catcher/enums/TaskTypeEnum.java
@@ -15,8 +15,8 @@ public enum TaskTypeEnum {
     EASTMONEYNET_STATEMENT ("eastmoney_statement", "财务报表", CycleEnum.DAY, new CycleRule () {
         public boolean decide () {
             Calendar calendar = Calendar.getInstance ();
-            //每天早上5点工作
-            return calendar.get (Calendar.HOUR_OF_DAY) == 5;
+            //每天晚上10点工作
+            return calendar.get (Calendar.HOUR_OF_DAY) == 22;
         }
     }),
 
@@ -26,8 +26,8 @@ public enum TaskTypeEnum {
     JUCAONET_COMPANY_LIST ("jucaonet_company_list", "股票列表", CycleEnum.DAY, new CycleRule () {
         public boolean decide () {
             Calendar calendar = Calendar.getInstance ();
-            //每天早上2点工作
-            return calendar.get (Calendar.HOUR_OF_DAY) == 2;
+            //每天早上6点工作
+            return calendar.get (Calendar.HOUR_OF_DAY) == 6;
         }
     }),
 
@@ -37,8 +37,8 @@ public enum TaskTypeEnum {
     JUCAONET_COMPANY_SHARECAPITAL ("jucaonet_company_sharecapital", "股本", CycleEnum.DAY, new CycleRule () {
         public boolean decide () {
             Calendar calendar = Calendar.getInstance ();
-            //每天早上4点工作
-            return calendar.get (Calendar.HOUR_OF_DAY) == 4;
+            //每天早上7点工作
+            return calendar.get (Calendar.HOUR_OF_DAY) == 7;
         }
     }),
 
@@ -53,8 +53,8 @@ public enum TaskTypeEnum {
             if (dayOfWeek == Calendar.SATURDAY || dayOfWeek == Calendar.SUNDAY) {
                 return false;
             }
-            //每天工作八小时
-            return calendar.get (Calendar.HOUR_OF_DAY) >= 8 && calendar.get (Calendar.HOUR_OF_DAY) <= 16;
+            //每天工作7小时
+            return calendar.get (Calendar.HOUR_OF_DAY) >= 9 && calendar.get (Calendar.HOUR_OF_DAY) <= 16;
         }
     }),
 
@@ -64,8 +64,12 @@ public enum TaskTypeEnum {
     SINAJS_HISTORY_TRADE_DETAIL ("sinajs_history_trade_detail", "本季度历史交易信息", CycleEnum.DAY, new CycleRule () {
         public boolean decide () {
             Calendar calendar = Calendar.getInstance ();
-            //每天晚上十点开始工作
-            return calendar.get (Calendar.HOUR_OF_DAY) >= 21;
+            //周末搞搞就行！！！
+            int dayOfWeek = calendar.get (Calendar.DAY_OF_WEEK);
+            if (dayOfWeek == Calendar.SATURDAY || dayOfWeek == Calendar.SUNDAY) {
+                return calendar.get (Calendar.HOUR_OF_DAY) >= 8;
+            }
+            return false;
         }
     }),
 
@@ -75,8 +79,12 @@ public enum TaskTypeEnum {
     SINAJS_LAST_QUATER_TRADE_DETAIL ("sinajs_last_quater_trade_detail", "前一季度历史交易信息", CycleEnum.DAY, new CycleRule () {
         public boolean decide () {
             Calendar calendar = Calendar.getInstance ();
-            //每天晚上十点开始工作
-            return calendar.get (Calendar.HOUR_OF_DAY) >= 22;
+            //周末搞搞就行！！！
+            int dayOfWeek = calendar.get (Calendar.DAY_OF_WEEK);
+            if (dayOfWeek == Calendar.SATURDAY || dayOfWeek == Calendar.SUNDAY) {
+                return calendar.get (Calendar.HOUR_OF_DAY) >= 9;
+            }
+            return false;
         }
     });
 
